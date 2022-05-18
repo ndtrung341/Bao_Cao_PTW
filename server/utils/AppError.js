5d47f4f007d079737d88f8ae0250e9ebca0f3b56
Nguyen Duy Trung <95160049+ndtrung341@users.noreply.github.com>
2022-05-18 08:31:49 +0700

    Delete server directory

--- server/utils/AppError.js
+++
@@ -1,8 +0,0 @@
-class AppError extends Error {
-	constructor(message, statusCode) {
-		super(message);
-		this.statusCode = statusCode || 500;
-		Error.captureStackTrace(this, this.constructor);
-	}
-}
-export default AppError;
